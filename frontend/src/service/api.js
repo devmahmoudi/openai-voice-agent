fb69928793d127dde34ca947b4db039fd7d86078
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-08-01 21:17:43 -0700

    refactor(frontend): add async type to getClientSecretKey and return full response instead of only secret_key

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const getClientSecretKey = (model) => {
+export const getClientSecretKey = async (model) => {
   try {
-    const URL = `http://localhost:8000/v1/agent/client_key?${model}`;
-    return axios.get(URL).data;
+    const URL = `http://127.0.0.1:8000/v1/agent/client_key?model=${model}`;
+    return await axios.get(URL);
   } catch (error) {
     console.error("Error fetching client secret key:", error);
     throw error;
